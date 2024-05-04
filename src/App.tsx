57d8318c08c250b0d3ef5bdbb5a83f860c7f501c
cshin <josa0005@naver.com>
2024-05-04 09:48:38 +0900

    chore: 소켓적용 실시간 차트 구현(중간)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { createChart, IChartApi, ISeriesApi } from 'lightweight-charts';
+import {
+  createChart,
+  IChartApi,
+  ISeriesApi,
+  MouseEventParams,
+} from 'lightweight-charts';
+import useNewData from './hooks/useNewData';
 
 interface UpbitCandle {
   market: string;
@@ -17,6 +23,7 @@ const App: React.FC = () => {
   const chartContainerRef = useRef<HTMLDivElement>(null);
   const chartRef = useRef<IChartApi | null>(null);
   const candleSeriesRef = useRef<ISeriesApi<'Candlestick'> | null>(null);
+  const newData = useNewData();
 
   //TODO
   // const handleMenuClick = () => {
@@ -37,37 +44,57 @@ const App: React.FC = () => {
         width: 700,
         height: 600,
       });
+
+      //pricescaleId를 생성
+      const priceScaleId = 'upbit-price-scale';
+
       candleSeriesRef.current = chartRef.current.addCandlestickSeries({
         priceLineVisible: true,
         lastValueVisible: true,
-        //차트의 범위를 지정
-        priceFormat: {
-          type: 'price',
-          precision: 0,
-          minMove: 0.01,
+        priceFormat: { type: 'price', precision: 0, minMove: 0.01 },
+        priceScaleId: priceScaleId,
+      });
+
+      // 해당 가격 축의 설정을 조정
+      chartRef.current.priceScale(priceScaleId).applyOptions({
+        autoScale: true,
+        invertScale: false,
+        alignLabels: true,
+        borderVisible: true,
+        borderColor: '#555ffd',
+        scaleMargins: {
+          top: 0.3,
+          bottom: 0.25,
         },
-        priceScale: {
-          mode: 1, // 상수 또는 계산된 값
-          autoScale: false,
-          invertScale: false,
-          alignLabels: true,
-          borderVisible: false,
-          borderColor: '#555ffd',
-          scaleMargins: {
-            top: 0.3,
-            bottom: 0.25,
-          },
-          entireTextOnly: true,
-          drawTicks: true,
-          tickMarkFormatter: price => {
-            // 이 함수는 축에 표시될 눈금 값을 커스터마이즈 할 수 있도록 합니다.
-            return Math.floor(price).toString();
-          },
+        entireTextOnly: true,
+        tickMarkFormatter: price => (price / 100000).toFixed(1),
+      });
+
+      //setup time scale formatter
+      chartRef.current.timeScale().applyOptions({
+        timeVisible: true,
+        secondsVisible: false,
+        tickMarkFormatter: (time, tickMarkType, locale) => {
+          const date = new Date(time * 1000);
+          switch (unit) {
+            case 'minute':
+            case 'hour':
+              return date.toLocaleTimeString(locale, {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false,
+              });
+            default:
+              return date.toLocaleDateString(locale, {
+                month: 'short',
+                day: 'numeric',
+              });
+          }
         },
       });
       setupTooltip(chartRef.current, candleSeriesRef.current);
     }
-  }, []);
+  }, [unit]); // Ensure formatter updates when unit changes
 
   useEffect(() => {
     const fetchData = async () => {
@@ -82,12 +109,16 @@ const App: React.FC = () => {
       };
 
       const selectedParam = urlParams[unit];
+      //newData 로 넘어올때 parsing이 되었는지를 판단해야된다면? - 데이터 정합성
+      //typeof 이나 instanceOf 으로 구분하는 함수 하나, 이에 대해 parsing 변경해줘야되는 값으로 구분 피료. - 에러 처리
+      //에러 처리 또한 필요하고, 에러 처리할때 사용자가 월/주/일/시간/분 중에 어떤값을 선택했는지도 필요함. - 일관성과 가독성
 
       try {
         const response = await axios.get(
           `https://api.upbit.com/v1/candles/${selectedParam.url}?market=KRW-BTC&count=${selectedParam.count}`
         );
         const sortedData = response.data
+          //TODO lightweight 차트 플립현상(소켓들어온 데이터가 가공되지 않고 적용후 약 0.2초정도 차트에 표시된다음 다시 정상으로 돌아옴.)
           .map((candle: UpbitCandle) => ({
             time: Math.floor(
               new Date(candle.candle_date_time_utc).getTime() / 1000
@@ -105,113 +136,161 @@ const App: React.FC = () => {
         console.warn('Error fetching data:', error);
       }
     };
-
+    //1.
     fetchData();
-  }, [unit]);
+    //2.
+    if (newData && candleSeriesRef.current) {
+      console.log('New data in component:', newData);
+      candleSeriesRef.current.update({
+        time: newData.time,
+        open: newData.open / 100000,
+        high: newData.high / 100000,
+        low: newData.low / 100000,
+        close: newData.close / 100000,
+      });
+    }
+  }, [newData, unit]);
 
-  //tooltip
-  // function setupTooltip(chart: IChartApi, series: ISeriesApi<'Candlestick'>) {
-  //   const tooltip = document.createElement('div');
-  //   tooltip.className = 'floating-tooltip';
-  //   document.body.appendChild(tooltip);
-  //   chart.subscribeCrosshairMove(function (param) {
-  //     if (
-  //       !param.time ||
-  //       param.point === undefined ||
-  //       !param.seriesPrices.get(series)
-  //     ) {
-  //       tooltip.style.display = 'none';
-  //     } else {
-  //       const priceData = param.seriesPrices.get(series);
-  //       tooltip.innerHTML = `
-  //         Date: ${new Date(param.time * 1000).toLocaleString()}<br>
-  //         Open: ${priceData.open}<br>
-  //         High: ${priceData.high}<br>
-  //         Low: ${priceData.low}<br>
-  //         Close: ${priceData.close}
-  //       `;
-  //       tooltip.style.display = 'block';
-  //       tooltip.style.left = `${Math.min(param.point.x + 20, window.innerWidth - tooltip.offsetWidth)}px`; // 화면 밖으로 나가지 않도록 조정
-  //       tooltip.style.top = `${Math.min(param.point.y + 20, window.innerHeight - tooltip.offsetHeight)}px`; // 화면 밖으로 나가지 않도록 조정
-  //     }
-  //   });
-  // }
   function setupTooltip(chart: IChartApi, series: ISeriesApi<'Candlestick'>) {
     const tooltip = document.createElement('div');
     tooltip.className = 'floating-tooltip';
+    tooltip.style.position = 'fixed';
     document.body.appendChild(tooltip);
 
-    chart.subscribeCrosshairMove(function (param) {
+    chart.subscribeCrosshairMove(function (
+      param: MouseEventParams<'Candlestick'>
+    ) {
       if (
         !param.time ||
         param.point === undefined ||
+        !param.seriesPrices ||
         !param.seriesPrices.get(series)
       ) {
         tooltip.style.display = 'none';
-      } else {
-        const priceData = param.seriesPrices.get(series);
-        tooltip.innerHTML = `
-          Date: ${new Date(param.time * 1000).toLocaleString()}<br>
-          Open: ${priceData.open}<br>
-          High: ${priceData.high}<br>
-          Low: ${priceData.low}<br>
-          Close: ${priceData.close}
-        `;
-        tooltip.style.display = 'block';
-        tooltip.style.left = `${Math.min(param.point.x + 20, window.innerWidth - tooltip.offsetWidth)}px`;
-        tooltip.style.top = `${Math.min(param.point.y + 20, window.innerHeight - tooltip.offsetHeight)}px`;
+        return;
       }
+      const priceData = param.seriesPrices.get(series);
+      if (!priceData) {
+        tooltip.style.display = 'none';
+        return;
+      }
+      tooltip.innerHTML = `
+        Date: ${param.time ? new Date(param.time * 1000).toLocaleString() : 'N/A'}<br>
+        Open: ${priceData.open}<br>
+        High: ${priceData.high}<br>
+        Low: ${priceData.low}<br>
+        Close: ${priceData.close}
+      `;
+      tooltip.style.display = 'block';
+      tooltip.style.left = `${param.point.x + 20}px`;
+      tooltip.style.top = `${param.point.y + 20}px`;
     });
   }
 
-  //zoomIn, zoomOut;
   const zoomIn = () => {
     if (chartRef.current) {
-      const priceScale = chartRef.current.priceScale();
-      priceScale.applyOptions({ autoScale: false });
-      const range = priceScale.priceRange();
+      const timeScale = chartRef.current.timeScale();
+      const range = timeScale.getVisibleLogicalRange();
       if (range) {
-        const diff = range.maxValue - range.minValue;
-        priceScale.setPriceRange({
-          minValue: range.minValue + diff * 0.1,
-          maxValue: range.maxValue - diff * 0.1,
+        const barsToZoom = (range.to - range.from) * 0.2; // Adjust zoom factor to 20%
+        timeScale.setVisibleLogicalRange({
+          from: range.from + barsToZoom,
+          to: range.to - barsToZoom,
         });
       }
     }
   };
   const zoomOut = () => {
     if (chartRef.current) {
-      const priceScale = chartRef.current.priceScale();
-      priceScale.applyOptions({ autoScale: false });
-      const range = priceScale.priceRange();
+      const timeScale = chartRef.current.timeScale();
+      const range = timeScale.getVisibleLogicalRange();
       if (range) {
-        const diff = range.maxValue - range.minValue;
-        priceScale.setPriceRange({
-          minValue: range.minValue - diff * 0.1,
-          maxValue: range.maxValue + diff * 0.1,
+        const barsToZoom = (range.to - range.from) * 0.5; // Adjust zoom factor to 50%
+        timeScale.setVisibleLogicalRange({
+          from: range.from - barsToZoom,
+          to: range.to + barsToZoom,
         });
       }
     }
   };
-  // const zoomIn = () => {
-  //   if (chartRef.current) {
-  //     chartRef.current.timeScale().zoomIn(2);
-  //   }
-  // };
-  // const zoomOut = () => {
-  //   if (chartRef.current) {
-  //     chartRef.current.timeScale().zoomOut(2);
-  //   }
-  // };
+  const zoomReset = () => {
+    if (chartRef.current) {
+      const timeScale = chartRef.current.timeScale();
+      // Reset to default zoom
+      timeScale.resetTimeScale();
+      timeScale.fitContent();
+    }
+  };
+
+  const setChartRange = (event: string) => {
+    const now = new Date();
+    now.setHours(0, 0, 0, 0); // Set time to midnight
+    const timeScale = chartRef.current?.timeScale();
+    if (timeScale) {
+      let from, to;
+      switch (event) {
+        case 'day':
+          from = now;
+          to = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+          break;
+        case 'weeks':
+          from = new Date(now.getTime() - now.getDay() * 24 * 60 * 60 * 1000);
+          to = new Date(from.getTime() + 7 * 24 * 60 * 60 * 1000);
+          break;
+        case 'month':
+          from = new Date(now.getFullYear(), now.getMonth(), 1);
+          to = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+          break;
+        default:
+          from = now;
+          to = new Date(now.getTime() + 24 * 60 * 60 * 1000); // Default to 1 day range
+          break;
+      }
+      timeScale.setVisibleRange({
+        from: from.getTime() / 1000,
+        to: to.getTime() / 1000,
+      });
+    }
+  };
 
   return (
     <div>
       <div ref={chartContainerRef} />
-      <button onClick={() => setUnit('minute')}>분</button>
-      <button onClick={() => setUnit('hour')}>시간</button>
-      <button onClick={() => setUnit('day')}>일</button>
-      <button onClick={() => setUnit('weeks')}>주</button>
-      <button onClick={() => setUnit('month')}>월</button>
+      <button
+        onClick={() => {
+          setUnit('minute');
+          setChartRange('minute');
+        }}>
+        분
+      </button>
+      <button
+        onClick={() => {
+          setUnit('hour');
+          setChartRange('hour');
+        }}>
+        시간
+      </button>
+      <button
+        onClick={() => {
+          setUnit('day');
+          setChartRange('day');
+        }}>
+        일
+      </button>
+      <button
+        onClick={() => {
+          setUnit('weeks');
+          setChartRange('weeks');
+        }}>
+        주
+      </button>
+      <button
+        onClick={() => {
+          setUnit('month');
+          setChartRange('month');
+        }}>
+        월
+      </button>
 
       {/* <dropdown overlay={menu} trigger={['click']}>
         <button>
@@ -219,8 +298,9 @@ const App: React.FC = () => {
         </button>
       </dropdown> */}
 
-      <button onClick={() => console.log('Zooming in') || zoomIn()}>+</button>
-      <button onClick={() => console.log('Zooming out') || zoomOut()}>-</button>
+      <button onClick={() => zoomIn()}>+</button>
+      <button onClick={() => zoomOut()}>-</button>
+      <button onClick={() => zoomReset()}>초기화</button>
     </div>
   );
 };
---
+++ src/hooks/useNewData.js
@@ -0,0 +1,96 @@
+import { useState, useEffect, useRef } from 'react';
+const UPBIT_URL = 'wss://api.upbit.com/websocket/v1';
+const useNewData = () => {
+  const [result, setResult] = useState();
+  const [timer, setTimer] = useState(false);
+  const data = [
+    { ticket: 'nexoneunji' },
+    { type: 'ticker', codes: ['KRW-BTC'], isOnlyRealtime: true },
+  ];
+  const ws = useRef(null);
+
+  useEffect(() => {
+    // timer 종료 시
+    if (timer) {
+      alert('만료되었습니다.');
+      ws.current.close();
+    }
+  }, [timer]);
+
+  useEffect(() => {
+    // 10분 지나면 종료 처리
+    setTimeout(
+      () => {
+        setTimer(true);
+      },
+      10 * 60 * 1000
+    );
+
+    ws.current = new WebSocket(UPBIT_URL);
+    ws.current.onopen = () => {
+      ws.current.send(JSON.stringify(data));
+    };
+    ws.current.onclose = () => {
+      console.log('DISCONNECTED');
+    };
+    ws.current.onmessage = async event => {
+      const text = await new Response(event.data).text();
+      const message = JSON.parse(text);
+      //Handle message...
+      const {
+        opening_price,
+        low_price,
+        high_price,
+        trade_price,
+        timestamp,
+        //trade_volume,
+      } = message;
+      //console.log('data received:', );
+      // Timestamp received: 1714714930721
+
+      const chartData = {
+        time: Math.floor(timestamp / 1000), // UNIX 타임스탬프 (초 단위)로 변환
+        open: opening_price,
+        high: high_price,
+        low: low_price,
+        close: trade_price,
+      };
+      console.log(
+        'data received : ' +
+          chartData.time.toString +
+          chartData.opening_price +
+          chartData.high_price +
+          chartData.low_price +
+          chartData.trade_price
+      );
+      setResult(chartData);
+      // setResult({
+      //   open: opening_price,
+      //   low: low_price,
+      //   high: high_price,
+      //   // 종가 = 현재가
+      //   close: trade_price,
+      //   volume: trade_volume,
+      //   // 오전 9시 기준 일봉
+      //   timestamp:
+      //     Math.floor(timestamp / 24 / 60 / 60 / 1000) * 24 * 60 * 60 * 1000,
+      //   turnover:
+      //     ((opening_price + low_price + high_price + trade_price) / 4) *
+      //     trade_volume,
+      // });
+    };
+    ws.current.onerror = event => {
+      console.log('Error', event);
+    };
+    ws.current.onclose = () => {
+      console.log('WebSocket Disconnected');
+    };
+    return () => {
+      ws.current.close();
+    };
+  }, [data]);
+  // 의존성 배열을 빈 배열로 설정하여 컴포넌트가 마운트될 때 한 번만 실행되도록 합니다.
+  return result;
+};
+
+export default useNewData;
