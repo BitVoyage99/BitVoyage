9532c4b8960a4fbf23dc3acd5818799d466cdac5
yoohmoon <121086010+yoohmoon@users.noreply.github.com>
2024-05-10 18:35:00 +0900

    chore: 변동시 하이라이트 박스 스타일링 추가

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -1,6 +1,16 @@
-import React, { useEffect, useRef, useState, memo } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useFetchMarketCode from '../hook/useFetchMarketCode';
 import useStore from '../stores/store';
+
+function usePrevious(value) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
 
 const SOCKET_URL = 'wss://api.upbit.com/websocket/v1';
 
@@ -68,6 +78,9 @@ const CoinList = () => {
   const { socketData, setSelectedCoin, selectedCoin } = useStore();
 
   // console.log('???tickerData??? ', tickerData);
+
+  const prevTickerData = usePrevious(tickerData);
+
   if (isLoading) {
     return <div>Loading market codes...</div>;
   }
@@ -128,6 +141,25 @@ const LogoImage = marketCode => {
     }
   }
 
+  function getChangeDirection(currentTicker, prevTicker) {
+    if (!prevTicker) return null; // No previous data, can't compare
+
+    const currentSignedChange = currentTicker.signed_change_price;
+    const prevSignedChange = prevTicker.signed_change_price;
+
+    // console.log(
+    //   `Current: ${currentSignedChange}, Previous: ${prevSignedChange}`
+    // );
+
+    if (currentSignedChange > prevSignedChange) {
+      return 'increased';
+    } else if (currentSignedChange < prevSignedChange) {
+      return 'decreased';
+    } else {
+      return 'unchanged';
+    }
+  }
+
   return (
     <div className="sticky top-18 bg-white h-screen w-full overflow-hidden max-w-xl mx-auto">
       <h1>{isConnected ? 'Connected' : 'Disconnected'}</h1>
@@ -143,60 +175,75 @@ const LogoImage = marketCode => {
       <div className="flex justify-between items-center bg-white w-full h-8 border-b border-gray-200 text-sm font-bold text-gray-600">
         <div className="w-12 pl-2 text-left">한글명</div>
         <div className="flex-1 text-right pr-2">현재가</div>
-        <div className="flex-1 text-right pr-2">상승률</div>
+        <div className="flex-1 text-right pr-2">전일 대비</div>
         <div className="w-1/4 text-right pr-3">거래대금</div>
       </div>
 
       <ul className="h-5/6 overflow-y-auto">
-        {tickerData.map((ticker, index) => (
-          <li
-            className="border-b border-gray-200 bg-gray-200"
-            key={index}
-            id={ticker.code}
-            onClick={handleClickCoin}
-            selected={selectedCoin === ticker.code}>
-            <button className="flex justify-between items-center w-full h-full text-left">
-              {/* 아이콘 */}
-              {/* <img src={LogoImage(ticker.code)} alt={` 로고`} /> */}
-              {/* <i
+        {tickerData.map((ticker, index) => {
+          const prevTicker = prevTickerData?.find(t => t.code === ticker.code);
+          const changeDirection = getChangeDirection(ticker, prevTicker);
+          const changeClass =
+            changeDirection === 'increased'
+              ? 'border border-solid border-rose-500'
+              : changeDirection === 'decreased'
+                ? 'border border-solid border-blue-500'
+                : '';
+
+          return (
+            <li
+              className={`border-b border-gray-200 bg-gray-200 `}
+              key={index}
+              id={ticker.code}
+              onClick={handleClickCoin}
+              selected={selectedCoin === ticker.code}>
+              <button className="flex justify-between items-center w-full h-full text-left">
+                {/* 아이콘 */}
+                {/* <img src={LogoImage(ticker.code)} alt={` 로고`} /> */}
+                {/* <i
                 className={`inline-block w-5 h-5 bg-[url(${LogoImage(ticker.code)})] bg-cover ml-1 mr-4`}></i> */}
-              {/* <i
+                {/* <i
                 className={`inline-block w-5 h-5 bg-[url('https://static.upbit.com/logos/${ticker.market.split('/')[0]}.png')] bg-cover ml-1 mr-4`}></i> */}
-              <div className="flex flex-col justify-center w-1/5 min-w-[55px]">
-                <strong className="block text-sm font-bold">
-                  {
-                    marketCodes.filter(code => code.market === ticker.code)[0]
-                      .korean_name
-                  }
-                </strong>
-                <span className="block text-xs">
-                  {/* {
+                <div className="flex flex-col justify-center w-1/5 min-w-[55px]">
+                  <strong className="block text-sm font-bold">
+                    {
+                      marketCodes.filter(code => code.market === ticker.code)[0]
+                        .korean_name
+                    }
+                  </strong>
+                  <span className="block text-xs">
+                    {/* {
                     marketCodes.filter(code => code.market === ticker.code)[0]
                       .market
                   } */}
-                  {ticker.code}
+                    {ticker.code}
+                  </span>
+                </div>
+                <div className={`${changeClass}`}>
+                  {/* <div className={`border border-solid ${changeClass}`}> */}
+                  {/* <div className={`border-b border-sky-500  ${changeClass}`}> */}
+                  <strong
+                    className={`block w-1/5 min-w-[55px] text-right align-middle text-sm font-bold ${getColorClass(ticker.change)}  `}>
+                    {ticker.trade_price.toLocaleString('ko-KR')}
+                  </strong>
+                </div>
+                <div
+                  className={`flex flex-col justify-center w-1/5 min-w-[55px] text-right ${getColorClass(ticker.change)}`}>
+                  <span className={`text-xs `}>
+                    {ticker.signed_change_rate > 0 ? '+' : null}
+                    {(ticker.signed_change_rate * 100).toFixed(2)}%
+                  </span>
+                  <span className="text-xs">
+                    {ticker.signed_change_price.toLocaleString('ko-KR')}
+                  </span>
+                </div>
+                <span className="flex flex-col justify-center text-xs w-1/4 text-right">
+                  {formatTradePriceToMillions(ticker.acc_trade_price_24h)}
                 </span>
-              </div>
-              <strong
-                className={`block w-1/5 min-w-[55px] text-right align-middle text-sm font-bold ${getColorClass(ticker.change)}`}>
-                {ticker.trade_price.toLocaleString('ko-KR')}
-              </strong>
-              <div
-                className={`flex flex-col justify-center w-1/5 min-w-[55px] text-right ${getColorClass(ticker.change)}`}>
-                <span className={`text-xs `}>
-                  {ticker.signed_change_rate > 0 ? '+' : null}
-                  {(ticker.signed_change_rate * 100).toFixed(2)}%
-                </span>
-                <span className="text-xs">
-                  {ticker.signed_change_price.toLocaleString('ko-KR')}
-                </span>
-              </div>
-              <span className="flex flex-col justify-center text-xs w-1/4 text-right">
-                {formatTradePriceToMillions(ticker.acc_trade_price_24h)}
-              </span>
-            </button>
-          </li>
-        ))}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
