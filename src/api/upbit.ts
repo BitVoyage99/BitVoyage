b2c483be56a4ff49db92177cfd7e32ce3e01e687
leehyeonseop <gustjq4146@naver.com>
2024-05-15 21:55:07 +0900

    chore: socket.io

--- src/api/upbit.ts
+++ src/api/upbit.ts
@@ -1,7 +1,13 @@
 import { upbitInstance } from '@/lib/axios';
-import { MarketData, TickerData } from '@/types/coin';
+import { MarketData, PeriodType, TickerData } from '@/types/coin';
 
 export const UPBIT_SOCKET_URL = 'wss://api.upbit.com/websocket/v1';
+
+type GetCandleParams = {
+  market: string;
+  period: PeriodType;
+  count?: number;
+};
 
 export const getAllMarket = async () => {
   try {
@@ -21,6 +27,25 @@ export const getTicker = async (markets: string[]) => {
     );
     if (response.status === 200) {
       return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getCandle = async ({
+  period,
+  market,
+  count = 1,
+}: GetCandleParams) => {
+  try {
+    const response = await upbitInstance.get(
+      `candles/${period}/1?market=${market}&count=${count}`
+    );
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error('Error');
     }
   } catch (error) {
     console.error(error);
